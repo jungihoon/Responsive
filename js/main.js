7de4927fa2796091b9259d87135e09fb9995cb24
wjsrlgns <a7s8d9789@naver.com>
2024-12-18 23:48:42 +0900

    반응형 작업

---
+++ js/main.js
@@ -0,0 +1,9 @@
+// <!-- Initialize Swiper -->
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
